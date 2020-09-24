98485387b8af249c8be34846daff856868457712
Stasnek <makstaser@gmail.com>
2020-09-25 00:48:48 +0400

    initial commit

---
+++ easy-wallet/src/main/kotlin/com/luxoft/training/msa/easywallet/EasyWalletApp.kt
@@ -0,0 +1,13 @@
+package com.luxoft.training.msa.easywallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan(basePackages = ["com.luxoft.training.msa"])
+@SpringBootApplication
+class EasyWalletApp
+
+fun main(args: Array<String>) {
+    runApplication<EasyWalletApp>(*args)
+}
---
+++ easy-wallet/src/main/kotlin/com/luxoft/training/msa/easywallet/controller/HelloController.kt
@@ -0,0 +1,14 @@
+package com.luxoft.training.msa.easywallet.controller
+
+import com.luxoft.training.msa.platform.service.UserService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController()
+class HelloController(private val userService: UserService) {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(defaultValue = "World") name: String): String {
+        return "Hello $name!"
+    }
+}
